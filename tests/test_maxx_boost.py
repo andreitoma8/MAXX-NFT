2fe42f0aa3409b2a6ef8e8b77e2f675eb94f2404
andreitoma8 <andrei.toma8@yahoo.com>
2022-07-04 20:53:04 +0300

    main

--- tests/test_maxx_boost.py
+++ tests/test_maxx_boost.py
@@ -9,27 +9,30 @@ def test_main():
     staking = MaxxStake.deploy({"from": owner})
     boost = MAXXBoost.deploy(amplify.address, staking.address, {"from": owner})
     staking.setMaxxBoost(boost.address, {"from": owner})
+
     # Participate in Liquidity Amplifier
     for i in range(0, 10):
         amplify.participate({"from": accounts[i]})
-    # Mint
+
+    # Mint NFTs
     boost.mint(123, {"from": owner})
     participants = amplify.getParticipants()
-    winner_one = boost.ownerOf(1)
     assert boost.ownerOf(1) == participants[123 % len(participants)]
     boost.mint(7, {"from": owner})
-    winner_two = boost.ownerOf(2)
-    assert winner_two == participants[7 % len(participants)]
+    assert boost.ownerOf(2) == participants[7 % len(participants)]
     boost.mint(1, {"from": owner})
-    winner_three = boost.ownerOf(3)
-    assert winner_three == accounts[1].address
-    # Use NFTs
+    assert boost.ownerOf(3) == accounts[1].address
+
+    # Check if NFT is marked as used after Staking
     assert boost.getUsedState(1) == False
     staking.stake(1, {"from": accounts[3]})
     assert boost.getUsedState(1) == True
+
+    # Check if staking with the used token is blocked
     with brownie.reverts():
         staking.stake(1, {"from": accounts[3]})
-    # NFT Metadata
+
+    # Check metadata change when token is used
     assert boost.tokenURI(2) == "available"
     staking.stake(2, {"from": accounts[7]})
     assert boost.tokenURI(2) == "used"
