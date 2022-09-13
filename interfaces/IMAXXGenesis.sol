9b4ccb7667359ebca8a4e483a905dadc52fef033
andreitoma8 <andrei.toma8@yahoo.com>
2022-09-13 09:59:33 +0300

    fixes #1

--- interfaces/IMAXXGenesis.sol
+++ interfaces/IMAXXGenesis.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-interface IMAXXBoost is IERC721 {
+interface IMAXXGenesis is IERC721 {
     function setUsed(uint256 _tokenId) external;
 
     function getUsedState(uint256 _tokenId) external view returns (bool);
 
-    function mint(uint256 _randomNumber) external;
-}
+    function mint(string memory _code, address _user) external returns (bool);
